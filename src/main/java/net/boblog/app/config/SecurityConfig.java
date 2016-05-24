12dcfac36c5911ebb2db6ed128347d57aad7d2f7
dave-harri <dave@markd.cn>
2016-05-24 23:22:05 +0800

    ..

--- src/main/java/net/boblog/app/config/SecurityConfig.java
+++ src/main/java/net/boblog/app/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
 /**
@@ -24,6 +25,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**").permitAll();
         http.csrf().disable();
-        http.sessionManagement().sessionAuthenticationStrategy(sessionAuthenticationStrategy);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                .sessionFixation().migrateSession()
+                .sessionAuthenticationStrategy(sessionAuthenticationStrategy);
     }
 }
